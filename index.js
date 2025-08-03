8ac9ce841055a63ed6a7a8ba1cf0f197086011a8
1dsevs <traxbrogaming@gmail.com>
2025-08-03 16:01:51 +0530

    Seperated the CSS to an external file

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const http = require('http');
 
-console.log("Program Started");
-console.log(process.platform);
-console.log(process.env.USER);
-
-
-
-http.createServer((req, res) => {
-}).listen(8080);
